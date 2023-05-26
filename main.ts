e7d5f0293d233d3837f8c3cc254ac37de0442ba1
rceccan2311 <132548157+rceccan2311@users.noreply.github.com>
2023-05-26 12:02:55 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(-25)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("POWER")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(25)
+})
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
